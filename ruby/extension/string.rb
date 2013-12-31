a8532707ca12d0462481520841fa774222b9f72f
Philippe Perret <philippe.perret@yahoo.fr>
2013-12-31 22:23:47 +0100

    Ajout de la méthode as_normalized_id

--- ruby/extension/string.rb
+++ ruby/extension/string.rb
@@ -20,6 +20,16 @@ class String
     self.tr(DATA_NORMALIZE[:from], DATA_NORMALIZE[:to])
   end
   
+  # Transforme la chaine en “id normalizé”, c'est-à-dire un 
+  # identifiant de type String, ne contenant que des lettres et
+  # des chiffres, avec capitalisation de la première lettre de
+  # chaque mot.
+  # Par exemple :
+  #     "Ça c'est l'été et mon titre" => "CaCestLeteEtMonTitre"
+  def as_normalized_id
+    self.normalize.gsub(/[^a-zA-Z0-9 ]/,'').downcase.split.collect{|m|m.capitalize}.join('')
+  end
+  
   # Retire les slashes
   # 
   def strip_slashes
